ef9f8cbb56eb92e885c33dc11ee8969f6c98e796
GabrielCremonese <gabrielfcremonese@gmail.com>
2026-07-26 21:27:18 -0300

    feat: initialize App component with layout sections and Vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import ComoEntrar from './components/ComoEntrar.tsx'
 import Contato from './components/Contato.tsx'
 import Footer from './components/Footer.tsx'
 import './index.css'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
